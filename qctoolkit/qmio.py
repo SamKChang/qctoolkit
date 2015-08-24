9334fe57368b464d26745db13d73a6dfc900a2e1
SamKChang <samio54321@gmail.com>
2015-08-24 10:25:16 +0200

    implement CUBE analysisi tools

--- qctoolkit/qmio.py
+++ qctoolkit/qmio.py
@@ -9,6 +9,7 @@ import numpy as np
 import utilities as ut
 from qctoolkit.io_format import *
 import qctoolkit.read_cube as rq
+import pylab as pl
 
 class CUBE(object):
   """
@@ -30,7 +31,7 @@ class CUBE(object):
     self.structure.Z = self.zcoord[:,0]
     self.structure.N = len(self.zcoord)
 
-  def integrate(self, flag):
+  def integrate(self, **kwargs):
     self._O = self.grid[0,1:4]
     self._vx = self.grid[1,1:4]
     self._vy = self.grid[2,1:4]
@@ -38,13 +39,70 @@ class CUBE(object):
     self._dV = np.linalg.norm(self._vx - self._O)\
               *np.linalg.norm(self._vy - self._O)\
               *np.linalg.norm(self._vz - self._O)
-    if flag==1:
-      return np.sum(np.ravel(self.data)) * self._dV
-    elif flag==2:
-      return np.sum(np.ravel(self.data**2)) * self._dV
+    if 'power' in kwargs:
+      power = kwargs['power']
+    else:
+      power = 1
+    return np.sum(np.ravel(self.data**power)) * self._dV
 
-  def plot(self, axis):
-    pass
+  def isCubic(self):
+    grid_vector = self.grid[1:,1:]
+    diff = np.linalg.norm(grid_vector, axis=1)\
+           - np.sum(grid_vector, axis=1)
+    if np.linalg.norm(diff) > 1E-8:
+      ut.exit("only orthorhombic grid is supported")
+      return False
+    else:
+      return True
+
+  def meshgrid(self):
+    if self.isCubic():
+      xi, yi, zi = (self.grid[0, i] for i in range(1,4))
+      dx, dy, dz = (self.grid[i, i] for i in range(1,4))
+      xf, yf, zf = (self.grid[i, 0]*self.grid[i, i]\
+                    for i in range(1,4))
+      # convert cube file Bohr to angstrom
+      xr = np.arange(xi, xf, dx)*0.529177249
+      yr = np.arange(yi, yf, dy)*0.529177249
+      zr = np.arange(zi, zf, dz)*0.529177249
+      return np.meshgrid(xr, yr, zr)
+    
+
+  def plot(self, **kwargs):
+    if self.isCubic():
+      if 'axis' in kwargs:
+        plot_axis = kwargs['axis']
+      else:
+        plot_axis = 0
+  
+      steps = [self.grid[i, i] for i in range(1,4)\
+               if not i == plot_axis+1]
+      axes = [i for i in range(3) if not i==plot_axis]
+      i = plot_axis+1
+      xi, xf, dx = (self.grid[0, i], 
+                    self.grid[i, 0]*self.grid[i, i],
+                    self.grid[i, i])
+      xout = np.arange(xi, xf, dx)*0.529177249
+      yout = np.sum(self.data, axis=tuple(axes))*np.prod(steps)
+      if 'name' in kwargs:
+        name = kwargs['name']
+      else:
+        name = 'density plot'
+      if 'plargs' in kwargs:
+        plargs = kwargs['plargs']
+      else:
+        plargs = []
+      if 'plkwargs' in kwargs:
+        plkwargs = kwargs['plkwargs']
+      else:
+        plkwargs = {}
+
+      pl.figure(name)
+      pl.plot(xout, yout, *plargs, **plkwargs)
+      if 'legend' in kwargs:
+        pl.legend([kwargs['legend']])
+      return xout, yout
+    
 
   def contour(self, axis, level):
     pass
