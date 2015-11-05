5d8368e6b1a8e896bd6eff89fda18b577bfeda22
SamKChang <samio54321@gmail.com>
2015-11-05 22:00:21 +0100

    implement 2 particle radial distribution function

--- qctoolkit/MD/c_extension/dlist2.c
+++ qctoolkit/MD/c_extension/dlist2.c
@@ -19,27 +19,31 @@ static PyObject* dlist_2(PyObject* self, PyObject* args){
   int *atom_list1;
   int *atom_list2;
   int nt, N, len0, len1, len2;
+  double dr, rho;
   double *data;
   double cell[3];
+  double cell_min;
 
   /* python output variables */
-  PyObject *np_matrix;
-  double *matrix;
+  PyObject *np_g;
+  PyObject *np_r;
+  double *g, *r;
   int mat_dim[1];
-  int i, j, k, t;
+  int i, j, k, t, itr;
   int I, J;
-  int itr = 0;
-  double Rij_t, dij, surf;
+  int size;
+  double Rij_t, dij, V;
 
   /*  parse numpy array and two integers as argument */
   //                      &PyArray_Type, &in_array, // O!, NpArray
-  if (!PyArg_ParseTuple(args, "OiiOOO", 
+  if (!PyArg_ParseTuple(args, "OiiOOOd", 
                         &in_array, // O, PyObject
                         &nt,       // i, integer
                         &N,        // i, integer
                         &l1_inp,   // O, PyObject
                         &l2_inp,   // O, PyObject
-                        &cell_inp  // O, PyObject
+                        &cell_inp, // O, PyObject
+                        &dr        // f, double
                        )) return NULL;
   if(in_array == NULL) return NULL;
 
@@ -49,7 +53,6 @@ static PyObject* dlist_2(PyObject* self, PyObject* args){
   /* access python list data */
   idata = PySequence_Fast(in_array, "expected a sequence");
   len0 = PySequence_Size(in_array);
-  nt = len0/3/N;
   data = (double*) malloc(nt * N * 3 * sizeof(double));
   for(i=0;i<len0;i++){
     item = PySequence_Fast_GET_ITEM(idata, i);
@@ -81,52 +84,75 @@ static PyObject* dlist_2(PyObject* self, PyObject* args){
     cell[i] = PyFloat_AsDouble(item);
   }
   Py_DECREF(cell_py);
+  cell_min = cell[0];
+  rho = len1 + len2;
+  for(i=0;i<3;i++){
+    if(cell[i]<cell_min) cell_min = cell[i];
+    rho /= cell[i];
+  }
   /***** end of input data construction *****/
 
-  /**************************
-  * construct output matrix *
-  **************************/
-  matrix = (double *) malloc(len1 * len2 * nt * sizeof(double));
+  /*********************
+  * construct output g *
+  *********************/
+  size = (int)(0.5*cell_min/dr) + 1;
+  g = (double *) malloc(size * sizeof(double));
+  r = (double *) malloc(size * sizeof(double));
+  for(i=0;i<size;i++){
+    r[i] = dr * (i+0.5);
+    g[i] = 0;
+  }
 
-#pragma omp parallel private(i,j,t,I,J,Rij_t) shared(matrix)
+#pragma omp parallel private(itr,i,j,t,I,J,Rij_t,dij) shared(g)
 {
   #pragma omp for
   for(t=0;t<nt;t++){
-    for(i=0;i<len1;i++){
-      I = i + t*N*3;
-      for(j=0;j<len2;j++){
-        J = j + t*N*3;
+    itr = 0;
+    for(i=0;i<len1-1;i++){
+      I = atom_list1[i]*3 + t*N*3;
+      for(j=i+1;j<len2;j++){
+        J = atom_list2[j]*3 + t*N*3;
         Rij_t = 0.0;
         for(k=0;k<3;k++){
           dij = data[I+k] - data[J+k];
-          if(dij>cell[k]) dij = cell[k] - dij;
+          dij = dij - cell[k]*round(dij/cell[k]);
           Rij_t += pow(dij, 2);
         }
-        surf = 4*PI*Rij_t;
-        matrix[j + i*len2 + t*len1*len2] = pow(Rij_t, 0.5)/surf;
+        Rij_t = sqrt(Rij_t);
+        if(Rij_t < 0.5*cell_min){
+          g[(int)(Rij_t/dr)] += 2.0;
+        }
+        itr++;
       }
     }
   }
 }
+  for(i=0;i<size;i++){
+    V = 4*PI*(pow((i+1)*dr,3) - pow(i*dr,3))/3.0;
+    g[i] /= V*nt*rho*len1;
+  }
 
-  mat_dim[0] = len1 * len2;
-  np_matrix = PyArray_SimpleNewFromData(1, mat_dim, 
-                                        NPY_DOUBLE, matrix);
-  /***** end of output matrix construction *****/
+  mat_dim[0] = size;
+  np_g = PyArray_SimpleNewFromData(1, mat_dim, NPY_DOUBLE, g);
+  np_r = PyArray_SimpleNewFromData(1, mat_dim, NPY_DOUBLE, r);
+  /***** end of output g construction *****/
 
   /*********************************
   * clean up and return the result *
   *********************************/
-  Py_INCREF(np_matrix);
-  return np_matrix;
+  Py_INCREF(np_g);
+  Py_INCREF(np_r);
+  return Py_BuildValue("OO", np_r, np_g);
 
   /*  in case bad things happen */
   fail:
-      Py_XDECREF(np_matrix);
-      return NULL;
+    Py_XDECREF(np_g);
+    Py_XDECREF(np_r);
+    return NULL;
 
   free(data);
-  free(matrix);
+  free(g);
+  free(r);
 }
 
 /*  define functions in module */
