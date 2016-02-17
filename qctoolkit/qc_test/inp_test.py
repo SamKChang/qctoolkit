742fe6024bdb8d30c0375b4a4259fa626c5615ef
SamKChang <ky.sam.chang@gmail.com>
2016-02-17 13:26:22 +0100

    clean up files

--- qctoolkit/qc_test/inp_test.py
+++ qctoolkit/qc_test/inp_test.py
@@ -8,15 +8,15 @@ files = sorted(glob.glob('data/molecules/*'))
 mols = []
 for i in files:
   print i
-  mols.append(qtk.QMInp(i, program='nwchem'))#, nuclear_charges=[[1,2,'H.test']]))
+  mols.append(qtk.QMInp(i, program='vasp'))#, nuclear_charges=[[1,2,'H.test']]))
 
 #mols[0].setAtom(1,element='C')
-out = mols[3].run(threads=4, save_restart=True, fix_molecule=False)
+#out = mols[3].run(threads=4, save_restart=True, fix_molecule=False)
 #print out
-#mols[3].write()
+mols[3].write()
 #out = mols[3].run(threads=4, save_restart=True)
-print out.n_basis
-print out.occupation
-print out.mo_eigenvalues
-print out.mo_coefficients
-print out.nuclear_repulsion
+#print out.n_basis
+#print out.occupation
+#print out.mo_eigenvalues
+#print out.mo_coefficients
+#print out.nuclear_repulsion
