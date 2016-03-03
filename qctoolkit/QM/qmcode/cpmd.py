555479b078391752a99b7735156859446911e807
SamKChang <ky.sam.chang@gmail.com>
2016-03-03 16:51:58 +0100

    remove prompt of download PP permision

--- qctoolkit/QM/qmcode/cpmd.py
+++ qctoolkit/QM/qmcode/cpmd.py
@@ -318,9 +318,9 @@ def PPCheck(xc, element, pp_file_str, **kwargs):
       if pp_file:
         new_pp = os.path.join(qtk.setting.cpmd_pp, pp_file_str)
         pp_content = urllib2.urlopen(pp_file).read()
-        qtk.prompt('pp file %s not found in %s. ' \
+        qtk.report('pp file %s not found in %s. ' \
                    % (pp_file_str, qtk.setting.cpmd_pp) + \
-                   'but found in cp2k page, download?')
+                   'but found in cp2k page, download now...')
         new_pp_file = open(new_pp, 'w')
         new_pp_file.write(pp_content)
         new_pp_file.close()
