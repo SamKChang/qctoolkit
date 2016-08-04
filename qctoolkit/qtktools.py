07cda4033ceb7d17516140a2ff78cde0e233c75a
SamKChang <ky.sam.chang@gmail.com>
2016-08-04 11:41:25 +0200

    start restructure package

---
+++ qctoolkit/qtktools.py
@@ -0,0 +1,16 @@
+import utilities
+import molecule
+import numpy
+import scipy
+import os
+import sys
+import copy
+import re
+import shutil 
+import periodictable
+import matplotlib
+
+Molecule = molecule.Molecule
+ut = utilities
+np = numpy
+sp = scipy
