153b5b75ad4f864557b3d6b76c9b8c7c14500b49
SamKChang <ky.sam.chang@gmail.com>
2016-09-20 12:44:12 +0200

    typo...

--- qctoolkit/QM/qmcode/gaussian.py
+++ qctoolkit/QM/qmcode/gaussian.py
@@ -142,7 +142,7 @@ class out(GaussianBasisOutput):
       data = outfile.readlines()
 
       pattern = re.compile(".*R*[ \n]*M*[ \n]*S*[ \n]*D[ \n]*=")
-      pattern2 = re.compile("[0-9].[0-9]")
+      pattern2 = re.compile("^.*[0-9]\.[0-9].*$")
       try:
         rmsd_list = filter(pattern.match, data)
         rmsd = filter(pattern2.match, rmsd_list)[-1]
