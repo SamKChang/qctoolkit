96981b43f60f5f95253ae3db577e07c1025c4de0
SamKChang <ky.sam.chang@gmail.com>
2016-09-22 23:34:41 +0200

    improve ML.pack interface

--- qctoolkit/ML/tools.py
+++ qctoolkit/ML/tools.py
@@ -8,7 +8,7 @@ def pack(data_list, **kwargs):
     max_N = max(map(len, Z))
   elif isinstance(data_list[0], qtk.QMOutput):
     typ = 'output'
-    Z = [o.molecule.Z for o in data_list]
+    Z = [o.molecule.Z for o in data_list if hasattr(o, 'molecule')]
     max_N = max(map(len, Z))
   else:
     qtk.warning("not supported datatype")
@@ -23,33 +23,37 @@ def pack(data_list, **kwargs):
 
   for i in range(len(data_list)):
     if typ == 'output':
-      molecule = data_list[i].molecule
+      if hasattr(data_list[i], 'molecule'):
+        molecule = data_list[i].molecule
+      else:
+        molecule = None
       Es.append(data_list[i].Et)
     elif typ == 'molecule':
       molecule = data_list[i]
 
-    zeroR = np.zeros([max_N - molecule.N, 3])
-    zeroZ = np.zeros(max_N - molecule.N)
-    xyz = np.vstack([molecule.R, zeroR]).tolist()
-    Z = np.hstack([molecule.Z, zeroZ])
-    if len(xyzs) == 0:
-      #xyzs = xyz
-      Zs = Z
-    else:
-      #xyzs = np.stack([xyzs,xyz])
-      Zs = np.vstack([Zs,Z])
-    xyzs.append(xyz)
-
-    if kwargs['output'] == 'extended_xyz':
-      xyzStr.append('%d\n' % molecule.N)
-      if typ == 'output':
-        xyzStr.append('%f\n' % data_list[i].Et)
-      elif typ == 'molecule':
-        xyzStr.append('\n')
-      for I in range(molecule.N):
-        r = molecule.R[I]
-        xyzStr.append('%-2s % 8.4f % 8.4f % 8.4f\n'\
-                      % (molecule.type_list[I], r[0], r[1], r[2]))
+    if molecule is not None:
+      zeroR = np.zeros([max_N - molecule.N, 3])
+      zeroZ = np.zeros(max_N - molecule.N)
+      xyz = np.vstack([molecule.R, zeroR]).tolist()
+      Z = np.hstack([molecule.Z, zeroZ])
+      if len(xyzs) == 0:
+        #xyzs = xyz
+        Zs = Z
+      else:
+        #xyzs = np.stack([xyzs,xyz])
+        Zs = np.vstack([Zs,Z])
+      xyzs.append(xyz)
+  
+      if kwargs['output'] == 'extended_xyz':
+        xyzStr.append('%d\n' % molecule.N)
+        if typ == 'output':
+          xyzStr.append('%f\n' % data_list[i].Et)
+        elif typ == 'molecule':
+          xyzStr.append('\n')
+        for I in range(molecule.N):
+          r = molecule.R[I]
+          xyzStr.append('%-2s % 8.4f % 8.4f % 8.4f\n'\
+                        % (molecule.type_list[I], r[0], r[1], r[2]))
   xyzs = np.array(xyzs)
   
   if len(Es) > 0:
