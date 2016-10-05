9112898b08d97740d9655a9cd2dd885f03f3fe64
SamKChang <ky.sam.chang@gmail.com>
2016-10-05 09:23:55 +0200

    implement coulomb_matrix

--- qctoolkit/ML/tools.py
+++ qctoolkit/ML/tools.py
@@ -1,6 +1,62 @@
 import qctoolkit as qtk
 import numpy as np
 
+def coulomb_matrix(mol, n = -2, size = 0, sort = True):
+  if size == 0:
+    size = mol.N
+  if size < mol.N:
+    qtk.exit("matrix size too small")
+  positions = mol.R
+  charges = mol.Z
+  differences = positions[:, np.newaxis, :] \
+              - positions[np.newaxis, :, :]
+  distances = np.sqrt((differences ** 2).sum(axis=-1))
+  distances[distances == 0] = np.nan # replace 0 for division
+  invR = (distances ** n)
+  invR[np.isnan(invR)] = 0 # change 0 back for getting diagonal
+  diag_mask = (invR == 0).astype(int)
+  charge_mask_Zij = charges[:, np.newaxis] \
+                  * charges[np.newaxis, :]
+  charge_mask_2p4 = 0.5 * ((charges[:, np.newaxis] \
+                            * charges[np.newaxis, :]) \
+                            * diag_mask) ** 1.2
+  cm = invR * charge_mask_Zij + charge_mask_2p4
+  if sort:
+    ind = np.argsort(cm.sum(axis=-1))
+    cm = cm[:, ind][ind]
+  out = np.zeros([size, size])
+  out[:cm.shape[0], :cm.shape[1]] = cm
+  return out
+
+def coulomb_matrices(positions, charges, n = -2, sort=True):
+  """
+  return 3D numpy array of sorted Coulomb matrix
+  """
+  differences = positions[..., :, np.newaxis, :] \
+              - positions[..., np.newaxis, :, :]
+  distances = np.sqrt((differences ** 2).sum(axis=-1))
+  distances[distances == 0] = np.nan
+  distances[distances == 0] = -1 # replace 0 for division
+  invR = (distances ** n)
+  invR[np.isnan(invR)] = 0 # change 0 back for getting diagonal
+  diag_mask = (invR == 0).astype(int)
+  charge_mask_Zij = charges[..., :, np.newaxis] \
+                  * charges[..., np.newaxis, :]
+  # diagonal part Z**2.4 / 2
+  charge_mask_2p4 = 0.5 * ((charges[..., :, np.newaxis] \
+                            * charges[..., np.newaxis, :]) \
+                            * diag_mask) ** 1.2
+  out = invR * charge_mask_Zij + charge_mask_2p4
+  if sort:
+    ind = np.argsort(out.sum(axis=-1))
+    # numpy fancy indexing with broadcast
+    out = out[
+      np.arange(len(out))[:, np.newaxis, np.newaxis], 
+      ind[:, :, np.newaxis], 
+      ind[:, np.newaxis, :]
+    ]
+  return out
+
 def pack(data_list, **kwargs):
   if isinstance(data_list[0], qtk.Molecule):
     typ = 'molecule'
