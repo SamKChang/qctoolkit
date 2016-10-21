ad3eae4592e88c0566c302c7d5101a65fddbff30
SamKChang <ky.sam.chang@gmail.com>
2016-10-21 18:44:50 +0200

    bug fix for submission script

--- qctoolkit/utilities/script/qsubqtk-maia.sh
+++ qctoolkit/utilities/script/qsubqtk-maia.sh
@@ -12,11 +12,13 @@ ROOT=$2
 NSLOTS=$3
 QSLOTS=$4
 FLAG=$5
-if [ $# -ge 4 ];then
+if [ $# -ge 5 ];then
   PREFIX=$6
 else
   PREFIX='q'
 fi
+
+CWD=`pwd`
 
 EXE=`echo $EXE|sed "s/\"//g"`
 
@@ -27,7 +29,7 @@ else
 fi
 
 if (( $QSLOTS > 8 ));then
-  paraSetup="#$ -pe orte* $QSLOTS"
+  paraSetup="#$ -pe mpi $QSLOTS"
 else
   paraSetup="#$ -pe smp $QSLOTS"
 fi
@@ -38,10 +40,10 @@ for dir in `ls -d */`; do
   cd $dir
   mydir=$PWD
   inp=`ls|grep -E "(inp|com|yaml)$"`
-  files=`ls|grep "files$"`
   BASE=${inp%.*}
   BCHK=$BASE".chk"
   out=`echo $inp|sed 's/\.[^\.]*$/.out/g'`
+  files=`ls|grep "files$"`
   if ! [ -z "$files" ];then
     sed -i '1iiomode 1' $inp
     inp=$files
@@ -54,13 +56,19 @@ for dir in `ls -d */`; do
 
   # job setup
   echo "#!/bin/bash"                                   > jobsub
-  #echo "module load OpenMPI/1.8.4-GCC-4.8.4"          >> jobsub
   echo "module load OpenMPI/1.6.4-GCC-4.7.2"          >> jobsub
   echo "module load hwloc/1.6.2-GCC-4.7.2"            >> jobsub
   echo "module load ABINIT/8.0.8-goolf-1.7.20"        >> jobsub
   echo "module load gaussian"                         >> jobsub
   echo "#$ -cwd"                                      >> jobsub
   echo "#$ -N $PREFIX${inp%.*}"                       >> jobsub
+#  echo 'NCPU=`nproc --all`'                           >> jobsub
+#  echo -n "if [[ $QSLOTS > "                          >> jobsub
+#  echo '$NCPU ]];then'                                >> jobsub
+#  echo "  #$ -pe mpi $QSLOTS"                         >> jobsub
+#  echo "else"                                         >> jobsub
+#  echo "  #$ -pe smp $QSLOTS"                         >> jobsub
+#  echo "fi"                                           >> jobsub
   echo "$paraSetup"                                   >> jobsub
   echo "#$ -S /bin/bash"                              >> jobsub
 
@@ -72,12 +80,16 @@ for dir in `ls -d */`; do
   echo "cd /tmp/$USER/$job"                           >> jobsub
 
   # mpi jobs
-  if [[ $NSLOT > 8 ]];then
-    echo -n "mpirun -np $NSLOTS -mca btl tcp,self "   >> jobsub
-  elif [[ $NSLOT > 1 ]];then
-    echo -n "mpirun -np $NSLOTS "                     >> jobsub
-  fi
+  echo 'NCPU=`nproc --all`'                           >> jobsub
+  echo -n "if [[ $NSLOTS > "                          >> jobsub
+  echo '$NCPU ]];then'                                >> jobsub
+  echo -n "  mpirun -np $NSLOTS -mca btl tcp,self "   >> jobsub
   echo "$EXE $inp > $out"                             >> jobsub
+  echo "elif [[ $NSLOTS > 1 ]];then"                  >> jobsub
+  echo "  mpirun -np $NSLOTS $EXE $inp > $out"        >> jobsub
+  echo "else"                                         >> jobsub
+  echo "  $EXE $inp > $out"                           >> jobsub
+  echo "fi"                                           >> jobsub
 
   # unified outputs
   # gaussian output
@@ -99,14 +111,28 @@ for dir in `ls -d */`; do
 
   # cleanup scratch
   echo "cd .."                                        >> jobsub
-  echo "cp -rT /tmp/$USER/$job $mydir"                >> jobsub
+  echo "cp -rT /tmp/$USER/$job/* $mydir"              >> jobsub
   echo "cd $mydir"                                    >> jobsub
   echo "rm -rf /tmp/$USER/$job"                       >> jobsub
 
   sed -i "/^%nproc/{s/=.*/=$NSLOTS/g}" $inp
-  sed -i "/^%chk/{s|=.*|=tmp.chk|g}" $inp
+  sed -i "/^%chk/{s|=.*|=$PWD/$BCHK|g}" $inp
   
-  qsub $FLAG jobsub
+  if [[ $EXE == "g09" ]];then
+    if [[ $FLAG == *"runtime"* ]];then
+      walltime=`echo $FLAG|sed 's|.*runtime *=\([0-9:]*\).*|\1|g'`
+    fi
+    if [ ${walltime+x} ];then
+      cmd="gsub $inp --runtime=$walltime"
+    else
+      cmd="gsub $inp"
+    fi
+    eval $cmd
+    echo "# $cmd" >> *.sh
+    echo "formchk $PWD/$BCHK" >> *.sh
+  else
+    qsub $FLAG jobsub
+  fi
   cd ..
 done
 cd ..
