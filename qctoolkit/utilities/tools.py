850f34084e405099f51389b6d582b060d6335698
samkchang <samio54321@gmail.com>
2017-03-30 15:50:24 +0200

    add md5sum check function

--- qctoolkit/utilities/tools.py
+++ qctoolkit/utilities/tools.py
@@ -4,6 +4,26 @@ import numpy as np
 import qctoolkit as qtk
 import yaml
 import pickle
+import hashlib
+
+def md5sum(fname):
+  def hash_bytestr_iter(bytesiter, hasher, ashexstr=True):
+      for block in bytesiter:
+          hasher.update(block)
+      return (hasher.hexdigest() if ashexstr else hasher.digest())
+  
+  def file_as_blockiter(afile, blocksize=65536):
+      with afile:
+          block = afile.read(blocksize)
+          while len(block) > 0:
+              yield block
+              block = afile.read(blocksize)
+  
+  out = hash_bytestr_iter(
+    file_as_blockiter(open(fname, 'rb')), hashlib.md5()
+  )
+
+  return out
 
 def flatten(container):
   for i in container:
