d11657e34ea826c2fbc4d9b8f19983268a638bd2
samkchang <samio54321@gmail.com>
2018-03-01 12:07:42 +0100

    finalize AO parsing

--- qctoolkit/utilities/cp2k_parser.py
+++ qctoolkit/utilities/cp2k_parser.py
@@ -1,9 +1,11 @@
 import qctoolkit as qtk
 import numpy as np
+import re
 
 def read_ao_file(path):
   with open(path) as f:
-    data = f.readlines()
+    #data = f.readlines()
+    data = f.read().splitlines()
 
   keys = filter(lambda x: 'matrix' in x.lower(), data)
 
@@ -14,8 +16,26 @@ def read_ao_file(path):
 
     key = k.replace('\n', '').lower()[1:]
 
-    matrix_str = filter(lambda x: len(x) > 2, data[ind_start:ind_end])
-    out[key] = matrix_str
+    matrix_str = filter(lambda x: len(x) > 2, data[ind_start+1:ind_end])
+    inds_parts = filter(lambda x: '              ' in x, matrix_str)
+
+    matrix_parts = []
+    for j, s in enumerate(inds_parts):
+      part_start = matrix_str.index(s) + 1
+      if s != inds_parts[-1]:
+        part_end = matrix_str.index(inds_parts[j+1])
+      else:
+        part_end = None
+      matrix_part = matrix_str[part_start:part_end]
+      part_data = np.array(
+        [terms.split()[4:] for terms in matrix_part]
+      ).astype(float)
+      matrix_parts.append(part_data)
+
+      if k == keys[-1] and s == inds_parts[-1]:
+        out['map'] = ["-".join(term.split()[2:4]) for term in matrix_part]
+
+    out[key] = np.hstack(matrix_parts)
 
   return out
 
